6a8b62cc1dad3991cf952e1e1e5be13b9b6a5536
Mio Nekoya <admin@mio9.sh>
2024-01-31 16:02:08 +0800

    stop panic

--- main.go
+++ main.go
@@ -273,12 +273,12 @@ func listRecords(config *Config, httpc *http.Client, zoneId string) ([]Cloudflar
 func getIp(config *Config) string {
 	resp, err := http.Get(config.IpProvider)
 	if err != nil {
-		panic(err)
+		return getIp(config)
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return getIp(config)
 	}
 
 	return strings.TrimSpace(string(body))
